89d0a7826ce6070e98df0550b2eda0748554eb7b
hhj120917 <1209172438@qq.com>
2019-11-17 00:30:25 +0800

    。

---
+++ python基础笔记html/利用os模块编写一个能实现dir -l输出的程序.py
@@ -0,0 +1,15 @@
+'''利用os模块编写一个能实现dir -l输出的程序。
+编写一个程序，能在当前目录以及当前目录的所有子目录下查找文件名包含指定字符串的文件，并打印出相对路径。'''
+from datetime import datetime
+import os
+
+pwd = os.path.abspath('.')
+
+print('      Size     Last Modified  Name')
+print('------------------------------------------------------------')
+
+for f in os.listdir(pwd):
+    fsize = os.path.getsize(f)
+    mtime = datetime.fromtimestamp(os.path.getmtime(f)).strftime('%Y-%m-%d %H:%M')
+    flag = '/' if os.path.isdir(f) else ''
+    print('%10d  %s  %s%s' % (fsize, mtime, f, flag))
