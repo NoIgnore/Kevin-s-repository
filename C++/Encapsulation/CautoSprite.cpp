caeb8ccb361e146ea6439fbfee7ca27b3a675bfa
hhj120917 <1209172438@qq.com>
2019-12-09 19:42:40 +0800

    C++

---
+++ C++/Encapsulation/CautoSprite.cpp
@@ -0,0 +1,48 @@
+#include "CautoSprite.h"
+
+CautoSprite::~CautoSprite(void)
+{
+}
+CautoSprite::CautoSprite(CautoSprite & a)
+{//复制构造函数
+	img=a.img;
+	x=a.x;
+	y=a.y;
+	dx=a.dx;
+	dy=a.dy;
+	width=a.width;
+	height=a.height;
+	winWidth=a.winWidth;
+	winHeight=a.winHeight;
+}
+CautoSprite::CautoSprite(char const *name,int x,int y,int dx,int dy,int w,int h,int wWidth,int wHeight)
+{//构造函数
+	loadImage(name,&img);
+	width=w;
+	height=h;
+	this->x=x;
+	this->y=y;
+	this->dx=dx;
+	this->dy=dy;
+	winWidth=wWidth;
+	winHeight=wHeight;
+}
+struct rect CautoSprite::getRect()
+{
+	struct rect r;
+	r.x=x;
+	r.y=y;
+	r.width=width;
+	r.height=height;
+	return r;
+}
+void CautoSprite::move()
+{
+	y+=dy;
+	if(y>winHeight-height) y*= 0;
+}
+
+void CautoSprite::drawScale(int w,int h)
+{
+	putImageScale(&img,x,y,w,h);
+}
--- C++/实验/sprite/inheritance/UsrSprite.h
+++ C++/实验/sprite/inheritance/UsrSprite.h
@@ -5,20 +5,20 @@
 
 class CUsrSprite :
 	public SpriteBase
-{
-	int score;
-public:
-	CUsrSprite(int x, int y, int width, int height, int dx, int dy, ACL_Image *img, rect r1);
-	CUsrSprite(CUsrSprite &sprite);
-	~CUsrSprite();
+	{
+		int score;
+	public:
+		CUsrSprite(int x, int y, int width, int height, int dx, int dy, ACL_Image *img, rect r1);
+		CUsrSprite(CUsrSprite &sprite);
+		~CUsrSprite();
 
-	void move(int x,int y);
-	void move(int key);
+		void move(int x,int y);
+		void move(int key);
 
-	int collision(rect r1);
-	int getScore();
-	void setScore(int s);
-	void addScore(int m);
-};
+		int collision(rect r1);
+		int getScore();
+		void setScore(int s);
+		void addScore(int m);
+	};
 
 #endif
