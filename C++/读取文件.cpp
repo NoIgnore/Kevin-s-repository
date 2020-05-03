15ec8cf410ac69209fcdf68902a67a7c5a6bbcec
NoIgnore <1209172438@qq.com>
2020-05-03 16:36:05 +0800

    .

--- C++/读取文件.cpp
+++ C++/读取文件.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 struct ip_list
 {
-	string address = "0"; 
+	string address = "0";
 	ip_list* next = nullptr;
 };
 struct ip_subnet
@@ -13,7 +13,7 @@ struct ip_subnet
 	ip_list* A = new ip_list;
 	ip_subnet* next = nullptr;
 };
-void readTxt(string file , ip_subnet* B)
+void readTxt(string file, ip_subnet* B)
 {
 	int abc = 1;
 	ifstream infile;
@@ -41,7 +41,7 @@ void readTxt(string file , ip_subnet* B)
 }
 int main()
 {
-	ip_subnet* first_head = new ip_subnet;
+	/*ip_subnet* first_head = new ip_subnet;
 	ip_list* headlist = new ip_list;
 	first_head->A->address = "123.123.343.545";
 	first_head->A->next = headlist;
@@ -55,29 +55,39 @@ int main()
 	if (first_head->A->next->address == first_head->A->address)
 	{
 		cout << "2" << endl;
-	}
-	ip_subnet* second = new ip_subnet;
-	readTxt("11.txt",second);
-	ip_list* second_headlist = new ip_list;
-	second_headlist = second->A;
-	while (second_headlist->address != "0")
-	{
-		cout << second_headlist->address << endl;
-		second_headlist = second_headlist->next;
-	}
+	}*/
+	//ip_subnet* second = new ip_subnet;
+	//readTxt("11.txt", second);//11.txt在当前目录下
+	//ip_list* second_headlist = new ip_list;
+	//second_headlist = second->A;
+	//while (second_headlist->address != "0")
+	//{
+	//	cout << second_headlist->address << endl;
+	//	cout << second_headlist->address[0] << endl;
+	//	second_headlist = second_headlist->next;
+	//}
+	string s1 = "123";
+	int a[3] = { 4,5,6 };
+	int b = atoi(s1.c_str());//string转换成int
+	string s2 = to_string(a[0]) + to_string(a[1]) + to_string(a[2]);//int转换成string
+	cout << s1[0] << s1[1] << s1[2] << endl;
+	cout << s2 << b << endl;
 	return 0;
 }
 /*
-123.123.343.545
-0
-1
-2
+new
+n
 12354465463
+1
 21356
+2
 786
+7
 43543
+4
 512312
+5
 123.234.345.456
-
+1
 按任意键关闭此窗口...
 */
