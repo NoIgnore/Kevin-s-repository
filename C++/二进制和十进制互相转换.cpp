dbbbdeeb0f4c06de96e65fd5d32bcde56262ab2f
NoIgnore <1209172438@qq.com>
2020-05-05 08:49:29 +0800

    .

--- C++/二进制和十进制互相转换.cpp
+++ C++/二进制和十进制互相转换.cpp
@@ -12,34 +12,40 @@ struct binstack
 int main()
 {
 	//bin to dec
-	string s1 = "1111";//dec=15
+	string s1 = "1010100111100";//to dec=5436
 	int length = s1.length();
 	int dec = 0;
-	int j = 0;
 	for (int i = 0; i <length; i++)
 	{
-		j = (s1[i] == '1' ? 1 : 0);
+		int j = (s1[i] == '1' ? 1 : 0);
 		dec += (j == 1 ? pow(2, length - i - 1) : 0);
 	}
 	cout << dec << endl;
 	//dec to bin
-	int test = 255;//bin=11111111
+	int test = 5436;//to bin=1010100111100
 	binstack* k1 = new binstack;
 	binstack* k2 = k1;
-	while (test != 0)
-	{
-		binstack* kk = new binstack;
-		kk->s = test % 2;
-		kk->next = k2;
-		k2 = kk;
-		test = test / 2;
-	}
-	binstack* k3 = k2;
 	string s2;
-	while (k3 != k1) 
+	if (test != 0)
 	{
-		s2 += to_string(k3->s);
-		k3 = k3->next;
+		while (test != 0)
+		{
+			binstack* kk = new binstack;
+			kk->s = test % 2;
+			kk->next = k2;
+			k2 = kk;
+			test = test / 2;
+		}
+		binstack* k3 = k2;
+		while (k3 != k1)
+		{
+			s2 += to_string(k3->s);
+			k3 = k3->next;
+		}
+	}
+	else
+	{
+		s2 = "0";
 	}
 	cout << s2 << endl;
 	return 0;
