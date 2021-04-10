c96f406820086f982ad41145bd35d5550b6da644
NoIgnore <1209172438@qq.com>
2021-04-10 19:36:47 +0800

    2021年04月10日 19:36:47

--- Python爬虫/hhj.py
+++ Python爬虫/hhj.py
@@ -3,10 +3,15 @@ from selenium import webdriver
 from time import sleep
 driver = webdriver.Firefox()
 def hhj():
-    driver.get("https://f.lingxi360.com/f?fid=QQwLeTNyQpUkC&utm_bccid=LXEHwOX2KbX4rqE2")
-    driver.find_element_by_id("btn_clear").click()
-    driver.find_element_by_id("name").send_keys("杰哥")
-    driver.find_element_by_id("member").send_keys("cypher")
+    driver.get("http://yck.mumuceo.com/")
+    hhj=r"form-control.span6"
+    Vmcode = driver.find_elements_by_class_name(hhj)
+    if len(Vmcode)==0:
+        driver.refresh()
+        return
+    #driver.find_element_by_id("btn_clear").click()
+    driver.find_elements_by_class_name(hhj)[0].send_keys("姓名")
+    driver.find_elements_by_class_name(hhj)[1].send_keys("学号")
     driver.find_element_by_id("button").click()
     driver.refresh()
 while(True):
