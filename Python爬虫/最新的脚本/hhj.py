f161241d12d5b1e34120900b2bf74dea5fb40cf9
NoIgnore <1209172438@qq.com>
2021-04-15 12:18:37 +0800

    2021年04月15日 12:18:37

--- Python爬虫/最新的脚本/hhj.py
+++ Python爬虫/最新的脚本/hhj.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 driver = webdriver.Edge(r'D:/Firefox/msedgedriver',{})
 def hhj():
-    link="https://ff.lingxi360.com/formfill?fid=imjEnaSiXxIS=&utm_bccid=LXExvNspO80BTohM&disable_autofill=0"
+    link="??"
     driver.get(link)
     hhj=r"form-control"
     Vmcode = driver.find_elements_by_class_name(hhj)
@@ -9,11 +9,11 @@ def hhj():
         driver.refresh()
         return
     try:
-        driver.find_elements_by_class_name(hhj)[1].send_keys("2018081311006")
-        driver.find_elements_by_class_name(hhj)[0].send_keys("洪浩杰")
+        driver.find_elements_by_class_name(hhj)[1].send_keys("2xxx")
+        driver.find_elements_by_class_name(hhj)[0].send_keys("xxx")
     except:
-        driver.find_element_by_id("name").send_keys("洪浩杰")
-        driver.find_element_by_id("new_item_1").send_keys("2018081311006")
+        driver.find_element_by_id("name").send_keys("xxx")
+        driver.find_element_by_id("new_item_1").send_keys("xxx")
     driver.find_element_by_id("button").click()
     driver.refresh()
 while(True):
