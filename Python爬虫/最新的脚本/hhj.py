04010894be045ef4fa3f45057842f7f215f09eea
NoIgnore <1209172438@qq.com>
2021-04-20 16:48:20 +0800

    2021年04月20日 16:48:20

--- Python爬虫/最新的脚本/hhj.py
+++ Python爬虫/最新的脚本/hhj.py
@@ -1,19 +1,25 @@
 from selenium import webdriver
 driver = webdriver.Edge(r'D:/Firefox/msedgedriver',{})
 def hhj():
-    link="??"
+    link="https://f.lingxi360.com/f?fid=n7=EbhLzHY9ua&utm_bccid=LXEOWiccxPKIskHf"
     driver.get(link)
     hhj=r"form-control"
     Vmcode = driver.find_elements_by_class_name(hhj)
     if len(Vmcode)==0:
         driver.refresh()
         return
+    name = '洪浩杰'
+    number = '2018081311006'
     try:
-        driver.find_elements_by_class_name(hhj)[1].send_keys("2xxx")
-        driver.find_elements_by_class_name(hhj)[0].send_keys("xxx")
+        driver.find_element_by_id("btn_clear").click()
     except:
-        driver.find_element_by_id("name").send_keys("xxx")
-        driver.find_element_by_id("new_item_1").send_keys("xxx")
+        trash=1
+    try:
+        driver.find_elements_by_class_name(hhj)[1].send_keys(number)
+        driver.find_elements_by_class_name(hhj)[0].send_keys(name)
+    except:
+        driver.find_element_by_id("name").send_keys(name)
+        driver.find_element_by_id("new_item_1").send_keys(number)
     driver.find_element_by_id("button").click()
     driver.refresh()
 while(True):
