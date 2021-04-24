d2ca1eaf817db6fecff77894fe344ddbba08f86c
NoIgnore <1209172438@qq.com>
2021-04-24 13:25:54 +0800

    2021年04月24日 13:25:54

--- Python爬虫/最新的脚本/hhj.py
+++ Python爬虫/最新的脚本/hhj.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 driver = webdriver.Edge(r'D:/Firefox/msedgedriver',{})
+hhj2=0
 def hhj():
     link="https://f.lingxi360.com/f?fid=n7=EbhLzHY9ua&utm_bccid=LXEOWiccxPKIskHf"
     driver.get(link)
@@ -8,19 +9,18 @@ def hhj():
     if len(Vmcode)==0:
         driver.refresh()
         return
-    name = '洪浩杰'
-    number = '2018081311006'
+    name = 'xxx'
+    number = 'xxxxxxxxx'
+    global hhj2
     try:
-        driver.find_element_by_id("btn_clear").click()
+        driver.find_element_by_id("new_item_1").send_keys(number)
+        driver.find_element_by_id("name").send_keys(name)
+        hhj2=1
     except:
-        trash=1
-    try:
         driver.find_elements_by_class_name(hhj)[1].send_keys(number)
         driver.find_elements_by_class_name(hhj)[0].send_keys(name)
-    except:
-        driver.find_element_by_id("name").send_keys(name)
-        driver.find_element_by_id("new_item_1").send_keys(number)
+        hhj2=1
     driver.find_element_by_id("button").click()
     driver.refresh()
-while(True):
+while(hhj2!=1):
     hhj()
