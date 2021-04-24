f122a365543800d4ca7164e691cd9b8fc6c80eb6
NoIgnore <1209172438@qq.com>
2021-04-24 19:58:33 +0800

    2021年04月24日 19:58:33

--- Python爬虫/最新的脚本/hhj.py
+++ Python爬虫/最新的脚本/hhj.py
@@ -1,5 +1,28 @@
 from selenium import webdriver
-driver = webdriver.Edge(r'D:/Firefox/msedgedriver',{})
+import os 
+import time 
+import ntplib 
+c = ntplib.NTPClient() 
+response = c.request('ntp.aliyun.com') 
+ts = response.tx_time 
+#_date = time.strftime('%Y-%m-%d',time.localtime(ts)) 
+_time = time.strftime('%X',time.localtime(ts+0.7))
+normaltime = '11:59:30'
+presentime = _time
+def timetransform(a):
+    h_to_s = (int(a[0])*10+int(a[1]))*3600
+    m_to_s = (int(a[3])*10+int(a[4]))*60
+    second = int(a[6])*10+int(a[7])
+    allsecond = h_to_s+m_to_s+second
+    return allsecond
+nn = timetransform(normaltime)
+pp = timetransform(presentime)
+minus = nn-pp
+print(minus)
+if(minus>=0):
+    time.sleep(minus)
+#driver = webdriver.Edge(r'D:/Firefox/msedgedriver',{})
+driver = webdriver.Firefox()
 hhj2=0
 def hhj():
     link="https://f.lingxi360.com/f?fid=n7=EbhLzHY9ua&utm_bccid=LXEOWiccxPKIskHf"
@@ -15,12 +38,13 @@ def hhj():
     try:
         driver.find_element_by_id("new_item_1").send_keys(number)
         driver.find_element_by_id("name").send_keys(name)
+        driver.find_element_by_id("button").click()
         hhj2=1
     except:
         driver.find_elements_by_class_name(hhj)[1].send_keys(number)
         driver.find_elements_by_class_name(hhj)[0].send_keys(name)
+        driver.find_element_by_id("button").click()
         hhj2=1
-    driver.find_element_by_id("button").click()
     driver.refresh()
 while(hhj2!=1):
     hhj()
