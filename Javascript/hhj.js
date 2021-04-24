42572c0a6a27742f088a5eeadd98999613f24772
NoIgnore <1209172438@qq.com>
2021-04-24 12:46:07 +0800

    2021年04月24日 12:46:07

--- Javascript/hhj.js
+++ Javascript/hhj.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.baidu.com/s?wd=autofill%E6%89%A7%E8%A1%8C%E4%BB%A3%E7%A0%81&pn=10&oq=autofill%E6%89%A7%E8%A1%8C%E4%BB%A3%E7%A0%81&tn=02003390_42_hao_pg&ie=utf-8&rsv_pq=bc0cd2c40001ee6e&rsv_t=e64bAEMboBvytCKFHN4qmNpzbekwWhlWEntYhVULo1T0Ys9TgJ33TuWsjMPThBezLZq0MNs5h5SL&rsv_page=1
+// @match        *://*f.lingxi360.com/*
 // @icon         https://www.google.com/s2/favicons?domain=baidu.com
 // @grant        none
 // @include     *://*f.lingxi360.com/*
@@ -12,39 +12,39 @@
 
 (function() {
     'use strict';
-    var now;
+    var get_id = 0;
+    var get_classname = 0;
     function doCompareTime()
     {
-        var b = new Date();
-        var tHour=b.getHours();
-        var tMinute=b.getMinutes();
-        var tSecond=b.getSeconds();
-        now = (tHour<10?"0"+tHour:tHour)+":"+(tMinute<10?"0"+tMinute:tMinute)+":"+(tSecond<10?"0"+tSecond:tSecond);
         var myname='xxx';
-        var mynumber='xxxxxxxx';
-        if(now>="12:00:00")
+        var mynumber='xxxxxxxxxxxxx';
+        try
         {
-            try
-            {
-                document.getElementById("new_item_1").value=mynumber;
-                document.getElementById("name").value=myname;
-                document.getElementById("button").click();
-                return;
-            }
-            catch(error)
-            {
-                var module1 = document.getElementsByClassName("form-control");
-                module1[1].value=mynumber;
-                module1[0].value=myname;
-                document.getElementById("button").click();
-                return;
-            }
-            finally
-            {
-                location.reload();
-            }
+            document.getElementById("new_item_1").value=mynumber;
+            document.getElementById("name").value=myname;
+            document.getElementById("button").click();
+            get_id = 0;
         }
-        location.reload();
+        catch(error)
+        {
+            get_id=1;
+        }
+        try
+        {
+            var module1 = document.getElementsByClassName("form-control");
+            module1[1].value=mynumber;
+            module1[0].value=myname;
+            document.getElementById("button").click();
+            get_classname = 0;
+        }
+        catch(error)
+        {
+            get_classname=1;
+        }
+        if(get_classname==1&&get_id==1)
+        {
+            location.reload();
+        }
     }
     setInterval(doCompareTime(),1000);
 })();
